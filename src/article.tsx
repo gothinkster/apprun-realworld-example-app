011c6d624dc6f5e4738ef8d5623e879599720a99
Yiyi Sun <yiyisun@gmail.com>
2017-09-06 13:50:42 -0400

    add comments to aarticle commponent

--- src/article.tsx
+++ src/article.tsx
@@ -1,6 +1,8 @@
 import app, { Component } from 'apprun';
 import { articles } from './api';
 import { IArticle } from './models';
+import Comments from './comment-list';
+
 class articleComponent extends Component {
   state = {
     article: null,
@@ -67,37 +69,7 @@ class articleComponent extends Component {
             </button>
           </div>
         </div>
-
-        <div className="row">
-          <div className="col-xs-12 col-md-8 offset-md-2">
-            <form className="card comment-form">
-              <div className="card-block">
-                <textarea className="form-control" placeholder="Write a comment..." rows="3"></textarea>
-              </div>
-              <div className="card-footer">
-                <img src="http://i.imgur.com/Qr71crq.jpg" className="comment-author-img" />
-                <button className="btn btn-sm btn-primary">
-                  Post Comment
-                </button>
-              </div>
-            </form>
-
-            {/* <div className="card">
-              <div className="card-block">
-                <p className="card-text">With supporting text below as a natural lead-in to additional content.</p>
-              </div>
-              <div className="card-footer">
-                <a href="" className="comment-author">
-                  <img src="http://i.imgur.com/Qr71crq.jpg" className="comment-author-img" />
-                </a>
-                &nbsp;
-                <a href="" className="comment-author">Jacob Schmidt</a>
-                <span className="date-posted">Dec 29th</span>
-              </div>
-            </div> */}
-
-          </div>
-        </div>
+        <Comments />
       </div>
     </div>
   }
--- src/comment-list.tsx
+++ src/comment-list.tsx
@@ -2,10 +2,45 @@ import app from 'apprun';
 import { IComment } from './models';
 
 function Comment({ comment }: { comment: IComment }) {
-
+  return <div className="card">
+    <div className="card-block">
+      <p className="card-text">
+        { comment.body }
+      </p>
+    </div>
+    <div className="card-footer">
+      <a className="comment-author">
+      <img src="comment.author.image" className="comment-author-img" />
+    </a>
+    &nbsp;
+    <a className="comment-author" href="['/profile', comment.author.username]">
+      { comment.author.username }
+    </a>
+    <span className="date-posted">
+      { comment.createdAt }
+    </span>
+    <span className="mod-options"> {/*hidden = !canModify*/}
+      <i className="ion-trash-a" onclick = "deleteClicked()" ></i>
+    </span>
+  </div>
+</div>
 }
 
 export default function ({ comments }: { comments: Array<IComment> }) {
-
-
+  return <div className="row">
+    <div className="col-xs-12 col-md-8 offset-md-2">
+      <form className="card comment-form">
+        <div className="card-block">
+          <textarea className="form-control" placeholder="Write a comment..." rows="3"></textarea>
+        </div>
+        <div className="card-footer">
+          <img src="http://i.imgur.com/Qr71crq.jpg" className="comment-author-img" />
+          <button className="btn btn-sm btn-primary">
+            Post Comment
+                </button>
+        </div>
+      </form>
+      {comments.map(comment => <Comment comment={comment} />)}
+    </div>
+  </div>
 }
